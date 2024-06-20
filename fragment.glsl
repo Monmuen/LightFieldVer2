9059ba49f74aa288da536cf36e354cbc8c4eecab
Fog1021 <92070637+Monmuen@users.noreply.github.com>
2024-06-20 18:18:08 +0800

    Update fragment.glsl

--- fragment.glsl
+++ fragment.glsl
@@ -1,5 +1,6 @@
 precision highp float;
 precision highp int;
+precision highp sampler2DArray;
 
 uniform sampler2DArray field;
 uniform vec2 camArraySize;
@@ -10,34 +11,26 @@ in vec2 vSt;
 in vec2 vUv;
 
 void main() {
-    vec4 color = vec4(0.0);
-    float colorCount = 0.0;
+  vec4 color = vec4(0.0);
+  float colorCount = 0.0;
 
-    // Check if the fragment is within the screen boundaries
-    if (vUv.x < 0.0 || vUv.x > 1.0 || vUv.y < 0.0 || vUv.y > 1.0) {
-        discard;
+  if (vUv.x < 0.0 || vUv.x > 1.0 || vUv.y < 0.0 || vUv.y > 1.0) {
+    discard;
+  }
+
+  for (float i = 0.0; i < camArraySize.x; i++) {
+    for (float j = 0.0; j < camArraySize.y; j++) {
+      float dx = i - (vSt.x * camArraySize.x - 0.5);
+      float dy = j - (vSt.y * camArraySize.y - 0.5);
+      float sqDist = dx * dx + dy * dy;
+      if (sqDist < aperture) {
+        float camOff = i + camArraySize.x * j;
+        vec2 focOff = vec2(dx, dy) * focus;
+        color += texture(field, vec3(vUv + focOff, camOff));
+        colorCount++;
+      }
     }
+  }
 
-    // Iterate over the camera array
-    for (float i = 0.0; i < camArraySize.x; i++) {
-        for (float j = 0.0; j < camArraySize.y; j++) {
-            float dx = i - (vSt.x * camArraySize.x - 0.5);
-            float dy = j - (vSt.y * camArraySize.y - 0.5);
-            float sqDist = dx * dx + dy * dy;
-
-            // Check if the current pixel is within the aperture
-            if (sqDist < aperture) {
-                float camOff = i + camArraySize.x * j;
-                vec2 focOff = vec2(dx, dy) * focus;
-
-                // Perform texture sampling
-                vec3 texCoord = vec3(vUv + focOff, camOff);
-                color += texture(field, texCoord);
-                colorCount++;
-            }
-        }
-    }
-
-    // Output final color averaged over the number of samples
-    gl_FragColor = vec4(color.rgb / colorCount, 1.0);
+  gl_FragColor = vec4(color.rgb / colorCount, 1.0);
 }
